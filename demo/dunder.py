5ef2d8f73a9cf6a8aab5cb7048d355d057a610e2
AnthonySinitsa <itonyemail@gmail.com>
2023-08-08 17:27:23 +0000

    command

--- demo/dunder.py
+++ demo/dunder.py
@@ -18,3 +18,11 @@ num2 = CustomNumber(10)
 # Test the custom dunder method
 result = num1 + num2
 print(result)  # Output: CustomNumber(15)
+
+class reverseString:
+  def __BOI__(self, string):
+    return string[::-1]
+  
+if __name__ == "__main__":
+  rev = reverseString()
+  print(rev.__BOI__("hello"))
