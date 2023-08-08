8ac8b783bd6f94f9470e109836e4be74e5785c92
AnthonySinitsa <itonyemail@gmail.com>
2023-08-08 17:31:25 +0000

    command

--- demo/dunder.py
+++ demo/dunder.py
@@ -1,24 +1,3 @@
-class CustomNumber:
-  def __init__(self, value):
-    self.value = value
-  
-  def __add__(self, other):
-    if isinstance(other, CustomNumber):
-      return CustomNumber(self.value + other.value)
-    else:
-      raise ValueError("no")
-
-  def __repr__(self):
-    return f"CustomNumber({self.value})"
-    
-# Create instances of CustomNumber
-num1 = CustomNumber(5)
-num2 = CustomNumber(10)
-
-# Test the custom dunder method
-result = num1 + num2
-print(result)  # Output: CustomNumber(15)
-
 class reverseString:
   def __BOI__(self, string):
     return string[::-1]
